5ffbdfc58216bfaf99e6e65be8b6f2a831261ee4
Avi-2509 <avi.srivastava@lybl.com>
2023-08-23 14:53:02 +0530

    Added code

--- models/profileInfo.sql
+++ models/profileInfo.sql
@@ -4,6 +4,6 @@ select
     u.userId as userId,
     u.profileList[0]."id" as profileId,
     u.profileList[0]."name" as name,
-    u.profileList[0]."email" as email,
+    u.profileList[0]."email" as em
     u.profileList[0]."phone" as phone
 from basic_profile.userprofile as u
