a0fbf6e9b573cfdd0b46ff4abdff4000f95bf9c8
Avi-2509 <avi.srivastava@lybl.com>
2023-08-23 13:31:36 +0530

    Added code

---
+++ models/testDb1.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+select
+    u.userId as userId,
+    u.profileList[0]."id" as profileId,
+    u.profileList[0]."name" as name,
+    u.profileList[0]."email" as email,
+    u.profileList[0]."phone" as phone
+from basic_profile.userprofile as u
